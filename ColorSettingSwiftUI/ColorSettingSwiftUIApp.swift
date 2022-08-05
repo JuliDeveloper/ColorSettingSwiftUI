c6b73efffb1d4aed23717eda0aaa2c7e72e208b6
JuliDeveloper <rja-97@yandex.ru>
2022-08-05 20:29:26 +0300

    Initial Commit

---
+++ ColorSettingSwiftUI/ColorSettingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSettingSwiftUIApp.swift
+//  ColorSettingSwiftUI
+//
+//  Created by Julia Romanenko on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSettingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
